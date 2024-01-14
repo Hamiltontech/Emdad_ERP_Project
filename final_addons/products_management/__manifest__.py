7d732b5ea4ef5c974c3075d451418c6b54e94213
ibraheem-areeda <ibraheem.areeda@gmail.com>
2024-01-14 16:30:47 +0300

    final addones created

---
+++ final_addons/products_management/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Products Management System',
+    'version': '1.3',
+    'website': 'https://www.emdadplatform.sa',
+    'category': 'Services/Project',
+    'sequence': 45,
+    'summary': 'Organize and plan your projects',
+    'depends': [
+        'base',
+    ],
+    'data':[
+        'views/products_management_views.xml',
+        'security/ir.model.access.csv',
+    ]
+
+}
