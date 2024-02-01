d1959721d0b51a58f8a83b305db62b1c56ee174f
ibraheem-areeda <ibraheem.areeda@gmail.com>
2024-02-01 13:05:00 +0300

    ibraheem edits

--- final_addons/emdad_finance/__manifest__.py
+++ final_addons/emdad_finance/__manifest__.py
@@ -8,7 +8,7 @@
     'license': 'LGPL-3',
     'category': '',
     'depends': [
-        'base', 'products_management', 'emdad_finance_product'
+        'base', 'products_management'
     ],
     'data': [
         'views/chart_of_account_view.xml',
